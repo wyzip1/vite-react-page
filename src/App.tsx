ed0f322158d483b4c7d8d5c8acf8b3f981a91fe2
wyzip <2362086452@qq.com>
2022-12-23 14:48:34 +0800

    主题样式修改

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,12 @@ import zhCN from "antd/locale/zh_CN";
 
 export default function App() {
   return (
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: { colorPrimary: "#155bd4", borderRadius: 0 },
+      }}
+    >
       <main style={{ width: "100%", height: "100vh" }}>
         <HashRouter>
           <RenderRouter routerList={routerList} />
