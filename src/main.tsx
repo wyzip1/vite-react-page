dadb6dc3075465408996e483d155f94063db12e6
wyzip <2362086452@qq.com>
2024-05-16 15:42:47 +0800

    nvm 自动切换，依赖更新最新

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { store } from "@/store";
+// import { store } from "@/store";
 import App from "./App";
 import "@/styles/tailwind.css";
 import "@/styles/index.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
+  // <Provider store={store}>
+  <HashRouter>
+    <App />
+  </HashRouter>
+  // </Provider>
 );
