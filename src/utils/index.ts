e709ef45b0902690d74e72a39fd7a45bf1ec766f
wyzip <2362086452@qq.com>
2024-07-12 10:21:22 +0800

    拆分copyInfo

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -246,48 +246,51 @@ export const createImg = (url: string) => {
   });
 };
 
+export const copyToEl = (el: Element) => {
+  const selection = window.getSelection()!;
+  if (selection.rangeCount > 0) selection.removeAllRanges();
+  const range = document.createRange();
+  document.body.appendChild(el);
+  range.selectNode(el); //传入dom
+  selection.addRange(range);
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+export const copyText = async (data: string) => {
+  // @ts-ignore
+  const permissions = await navigator.permissions.query({ name: "clipboard-write" });
+  if (permissions.state === "granted") {
+    await navigator.clipboard.writeText(data);
+    return;
+  }
+
+  const text = document.createElement("span");
+  text.innerText = data;
+  copyToEl(text);
+};
+
+export const copyImg = async (data: { type: "img"; url: string }) => {
+  // @ts-ignore
+  const permissions = await navigator.permissions.query({ name: "clipboard-write" });
+  if (permissions.state === "granted") {
+    const res = await fetch(data.url, { headers: { "Response-Type": "blob" } });
+    const blob = await res.blob();
+    await navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })]);
+    return;
+  }
+
+  const img = await createImg(data.url);
+  copyToEl(img);
+};
+
 export const copyInfo = async (data?: string | { type: "img"; url: string }) => {
   try {
     if (!data) return;
-    // @ts-ignore
-    const permissions = await navigator.permissions.query({ name: "clipboard-write" });
-    if (permissions.state === "granted") {
-      if (typeof data === "string") await navigator.clipboard.writeText(data);
-      else if (data.type === "img") {
-        const res = await fetch(data.url, { headers: { "Response-Type": "blob" } });
-        const blob = await res.blob();
-
-        await navigator.clipboard.write([
-          new ClipboardItem({
-            [blob.type]: blob,
-          }),
-        ]);
-      }
-    } else {
-      const createCopyDom = async () => {
-        if (typeof data === "string") {
-          const text = document.createElement("span");
-          text.innerText = data;
-          return text;
-        } else {
-          const img = await createImg(data.url);
-          return img;
-        }
-      };
-      const copyDom = await createCopyDom();
-      const selection = window.getSelection()!;
-      if (selection.rangeCount > 0) selection.removeAllRanges();
-      const range = document.createRange();
-      document.body.appendChild(copyDom);
-      range.selectNode(copyDom); //传入dom
-      selection.addRange(range);
-      document.execCommand("copy");
-      document.body.removeChild(copyDom);
-    }
+    typeof data === "string" ? copyText(data) : copyImg(data);
     message.success("复制成功");
   } catch (error) {
     console.log("copy error:", error);
-
     message.error("复制失败");
   }
 };
